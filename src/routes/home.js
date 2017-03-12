1a6d36aabff3b365fc7489d18880b319facce597
Thomas Munzer <tmunzer@aerohive.com>
2017-03-12 17:21:05 +0100

    remove debug logs

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    console.info("\x1b[32minfo\x1b[0m:",req.user);
     res.render('home', {title: 'Attendance'});
-
 });
 
 module.exports = router;
